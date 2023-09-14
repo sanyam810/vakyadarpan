be50f3e9fdb6b68cc9589ed31891dca3b94f8224
sanyam810 <sanyamsaini081@gmail.com>
2023-09-14 22:29:34 +0530

    implemented navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,12 @@ const App = () => {
   return (
     <div className="min-h-screen">
      <div className="gradient-bg-welcome">
+      <div className="flex flex-col gap-28">
         <Navbar />
         <Welcome />
-        <Services />
+        
+      </div>
+      <Services />
      </div>
      <div>
         
